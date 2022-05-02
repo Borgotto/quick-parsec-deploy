e0105f3ed2cab28327eff4f8e5bf6427aed6f4ea
Emanuele Borghini <manuborg2002@gmail.com>
2022-05-02 23:51:22 +0200

    write main script

--- main.ps1
+++ main.ps1
@@ -1,1 +1,14 @@
+# clean previous files if needed
+Remove-Item .\parsec.zip -ErrorAction Ignore -Force
+Remove-Item .\parsec -Force -Recurse -ErrorAction Ignore
 
+# download the latest build of the portable version (32 bit) of parsec
+Invoke-WebRequest 'https://builds.parsecgaming.com/package/parsec-flat-windows32.zip' -OutFile .\parsec.zip
+
+# extract the downloaded zip and then delete it
+Expand-Archive .\parsec.zip .\parsec
+Remove-Item .\parsec.zip
+
+# run parsec
+Set-Location .\parsec
+Start-Process parsecd.exe
