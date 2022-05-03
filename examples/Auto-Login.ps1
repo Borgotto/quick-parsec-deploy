c8d4e15d60e3dfa3dcdb7461d8609a9162093053
Emanuele Borghini <manuborg2002@gmail.com>
2022-05-03 21:31:34 +0200

    add examples

---
+++ examples/Auto-Login.ps1
@@ -0,0 +1,2 @@
+# this won't actually auto-login to any account as .\config_files\user.bin is empty
+..\main.ps1 -LoginFile ".\config_files\user.bin"
---
+++ examples/Install-Server.ps1
@@ -0,0 +1,1 @@
+..\main.ps1 -ConfigFile ".\config_files\server_config.txt" -OutFolder "parsec-server"
